7eadd2ab83824861854c217c5b797c504c4ffbff
Raquel Villaverde Palma <raquelv85@gmail.com>
2022-03-17 13:45:48 +0100

    change languages and timezone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { SalesModule } from './sales/sales.module';
 
 //cambiar idioma
 import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common'
 registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 @NgModule({
   declarations: [
